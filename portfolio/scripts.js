4be98cf9e53f262ac8d1ca09c959545f0f9cc18d
cj738 <cjames351@gmail.com>
2025-04-09 22:23:58 -0500

    add background image, set vh to 100 for each section, add nav bar

---
+++ portfolio/scripts.js
@@ -0,0 +1,23 @@
+
+  const sections = document.querySelectorAll("section, header, footer");
+  const navLinks = document.querySelectorAll("nav a");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop - 100;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === `#${current}`) {
+        link.classList.add("active");
+      }
+    });
+  });
+
